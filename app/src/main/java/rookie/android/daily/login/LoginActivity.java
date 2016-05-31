a6400ff67fabccd0c961f34e4744d321696eae1b
raledon <863814297@qq.com>
2016-05-31 11:53:17 +0800

    实现联系人界面

--- app/src/main/java/rookie/android/daily/login/LoginActivity.java
+++ app/src/main/java/rookie/android/daily/login/LoginActivity.java
@@ -3,6 +3,7 @@ package rookie.android.daily.login;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.Window;
 
